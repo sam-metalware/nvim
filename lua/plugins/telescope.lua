a016e57cfd53fec4683ec8bd0575913d1e857729
Samuel Lichtenheld <samuel.lichtenheld@gmail.com>
2025-02-01 17:00:04 -0600

    Make telescope find files leader leader

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,7 +3,7 @@ return {
   -- replace all Telescope keymaps with only one mapping
   keys = function()
     return {
-	    {"<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Telescope Find Files" },
+	    {"<leader><leader>", "<cmd>Telescope find_files<cr>", desc = "Telescope Find Files" },
 	    {"<leader>fg", "<cmd>Telescope live_grep<cr>",  desc = "Telescope Live Grep"  },
 	    {"<leader>fb", "<cmd>Telescope buffers<cr>",    desc = "Telescope buffers"    },
 	    {"<leader>fh", "<cmd>Telescope help_tags<cr>",  desc = "Telescope help tags"  },
