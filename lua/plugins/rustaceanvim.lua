516fdd96289ffc599ebdf8451a8e32ce6b656f92
Samuel Lichtenheld <samuel.lichtenheld@gmail.com>
2025-11-18 14:51:31 -0800

    fix rustacean keymaps

--- lua/plugins/rustaceanvim.lua
+++ lua/plugins/rustaceanvim.lua
@@ -7,12 +7,12 @@ vim.g.rustaceanvim = {
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
       vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
       vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-      vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, opts)
+      vim.keymap.set('n', 'gy', vim.lsp.buf.type_definition, opts)
       vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
 
       -- Documentation
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-      vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+      vim.keymap.set('n', 'gK', vim.lsp.buf.signature_help, opts)
 
       -- Code actions
       vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
@@ -28,7 +28,7 @@ vim.g.rustaceanvim = {
       vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
       vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
       vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts)
-      vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+      vim.keymap.set('n', 'cd', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 
       -- Debuggables
       vim.keymap.set("n", "<leader>rd", function()
