975d854228c33068c85247dcb2f863ff60841cd6
Samuel Lichtenheld <samuel.lichtenheld@gmail.com>
2025-12-05 10:49:18 -0800

    Add diffview plugin

---
+++ lua/plugins/diffview.lua
@@ -0,0 +1,7 @@
+return {
+    "sindrets/diffview.nvim",
+    -- keys = function()
+    --     return {
+    --         {"<leader>gd", "<cmd>Git diff<cr>", desc = "Fugitive git diff"},
+    --     }
+}
--- lua/plugins/vim-fugitive.lua
+++ lua/plugins/vim-fugitive.lua
@@ -3,7 +3,6 @@ return {
     keys = function()
       return {
           {"<c-g>", "<cmd>Git blame<cr>", desc = "Fugitive git blame"},
-          {"<leader>gd", "<cmd>Git diff<cr>", desc = "Fugitive git diff"},
       }
     end,
 }
